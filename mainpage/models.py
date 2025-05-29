ec1a81c793714f69d88740da6955c9c375bac065
philipp kartashov <filkart80@gmail.com>
2025-05-29 20:51:19 +0300

    дз с таблицей

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,11 @@
+# article/models.py
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.CharField(max_length=100000)
+    dt = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
