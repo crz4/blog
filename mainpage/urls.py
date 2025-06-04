1024b892800ddbd19304267e69b7d46b75e5dd5c
philipp kartashov <filkart80@gmail.com>
2025-06-04 13:07:06 +0300

    логин и пароль

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path('', views.main , name='mainpage'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
